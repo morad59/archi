6b4038fa670bcbf0cd39f397b807948f18eed867
Morad Derouich <morad.derouich@laposte.net>
2025-01-09 22:13:12 +0100

    Update send_sms.php

--- sms/vendor/send_sms.php
+++ sms/vendor/send_sms.php
@@ -3,13 +3,13 @@ require __DIR__ . '/vendor/autoload.php';
 use Twilio\Rest\Client;
 
 // Your Account SID and Auth Token from twilio.com/console
-$account_sid = 'ACe4676d7bcf84d104a546717d7d38057e';
-$auth_token = 'c7bcf7fe8cc17229e357f628d3fb4887';
+$account_sid = '***********************************';
+$auth_token = '******************************';
 // In production, these should be environment variables. E.g.:
 // $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
 
 // A Twilio number you own with SMS capabilities
-$twilio_number = "+15405017470";
+$twilio_number = "+154050174**";
 
 $client = new Client($account_sid, $auth_token);
 $client->messages->create(
