341b3dbdb8fb7170ddfe8036162d93070cda996e
Morad Derouich <morad.derouich@laposte.net>
2025-01-09 22:05:18 +0100

    Update config_mail.php

--- mail/config_mail.php
+++ mail/config_mail.php
@@ -5,8 +5,8 @@ return [
     /*'server_smtp_mail' => '{imap.ionos.fr:993/imap/ssl}INBOX',*/ 
     'id_mail' => 'morad.derouich@laposte.net',
     //'id_mail' => 'contact@appliops.fr', */ 
-    'api_key_mail' => 'Hazebrouck111a'
-    //'api_key_mail' => 'Encule1985&&a&'*/ 
+    'api_key_mail' => '**********'
+    //'api_key_mail' => '********'*/ 
 ];
 
 ?>
