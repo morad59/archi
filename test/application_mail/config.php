ee5e6cfa55e3ea3c18cffbb274fc747ab74429a4
Morad Derouich <morad.derouich@laposte.net>
2025-02-25 01:36:56 +0100

    Update config.php
    secUre process

--- test/application_mail/config.php
+++ test/application_mail/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'pw_db' => 'Encule1985&&a&',
-    'id_db' => 'phpmyadmin',
-    'na_db' => 'appliops_mssante_appliops',
+    'pw_db' => '$$$$$$$$$$$$$$',
+    'id_db' => '**********',
+    'na_db' => '********_$$$$$$$_&&&&&&&&',
 ];
 
